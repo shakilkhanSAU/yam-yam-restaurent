d7a65fcc4e65129537fde919ae6de459bb65a9d8
nurboss <nurmohammod993@gmail.com>
2022-02-14 13:01:51 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
     <Home></Home>      
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/home.css'
+import Banner from './Banner';
+
+const home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default home;
