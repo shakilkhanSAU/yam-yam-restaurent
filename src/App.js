5c113048c545bd90784d570a31f7f6d34f9dcdc1
nurboss <nurmohammod993@gmail.com>
2022-02-14 11:22:06 +0600

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is my first line of program</h1>
-      <h3>Now im pushing code from development branch</h3>
+    <Home></Home>      
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            <h1>This is from home</h1>
         </div>
     );
 };
