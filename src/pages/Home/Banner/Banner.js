55981991a8ca414cacc4d57d009bd675f13fabbb
nurboss <nurmohammod993@gmail.com>
2022-02-15 14:54:21 +0600

    services completed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import banner_img from '../../../images/banner.png';
 import '../../style/banner.css';
 
 
 const Banner = () => {
     return (
-        <div>
+        <div className='container'>
 
-            <Container>
+            
                 <div className="row view d-flex justify-content-center align-items-center flex-lg-row flex-column-reverse">
                     <div className="col-lg-6 col-md-12 col-sm-12">
                         <h1 className='main-header'>Delicious food for <br />your cravings</h1>
@@ -19,7 +18,7 @@ const Banner = () => {
                         <img className='img-fluid' src={banner_img} alt="" />
                     </div>
                 </div>
-            </Container>
+            
         </div>
     );
 };
