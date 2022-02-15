9ae6115cabc8d0875f0ddd63f779c6bd5b05fdd0
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-02-15 22:20:42 +0600

    naming convention

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../../shared/Footer/Footer';
 import Header from '../../shared/Header/Header';
 import Banner from '../Banner/Banner';
-
+import Services from '../Services/Services'
 
 const home = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Services></Services>
             <Footer></Footer>
         </div>
     );
--- src/pages/Home/Services/Services.js
+++ src/pages/Home/Services/Services.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServicesCart from './ServiceCart';
 
-const Services = () => {
+
+const Service = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/shakilkhanSAU/yam-yam-restaurent/development/public/data.json')
+            .then(result => result.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
-            <h3>this is services</h3>
+        <div className="container my-5">
+            <div className="row">
+                {
+                    services.map(data => <ServicesCart
+                        key={data.id}
+                        data={data}
+                    ></ServicesCart>)
+                }
+            </div>
         </div>
     );
 };
 
-export default Services;
+export default Service;
