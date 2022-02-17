02c75e9e13343a5efe26514ecf0ab0af8cb642a3
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-02-18 00:09:16 +0600

    manage order and add Product added

--- src/pages/MyOrder/MyOrder.js
+++ src/pages/MyOrder/MyOrder.js
@@ -4,47 +4,45 @@ import MyOrderCart from './MyOrderCart';
 
 const MyOrder = () => {
     const [myOrder, setMyOrder] = useState([]);
-    const {user} = useAuth();
+    const { user } = useAuth();
+    const email = user?.email;
     useEffect(() => {
-        const url = `https://young-wildwood-21988.herokuapp.com/myorder/${user.email}`;
+        const url = `https://young-wildwood-21988.herokuapp.com/myorder/${email}`;
         fetch(url)
-        .then(res => res.json())
-        .then(data => setMyOrder(data));
-
-    }, [user.email]);
-    
-    
+            .then(res => res.json())
+            .then(data => setMyOrder(data));
+    }, [email]);
 
     const handleDeleteOrder = id => {
         fetch(`https://young-wildwood-21988.herokuapp.com/deleteOrde/${id}`, {
             method: 'DELETE',
-            headers: {'Content-Type': 'application/json'}
+            headers: { 'Content-Type': 'application/json' }
         })
-        .then(res => res.json())
-        .then(result => {
-            if(result.acknowledged){
-            alert('Are you Sure you want to cancel you order.')
-        const remainig = myOrder.filter(service => service._id !== id);
-            setMyOrder(remainig);
-            }
-            
-        });
+            .then(res => res.json())
+            .then(result => {
+                if (result.acknowledged) {
+                    alert('Are you Sure you want to cancel you order.')
+                    const remainig = myOrder.filter(service => service._id !== id);
+                    setMyOrder(remainig);
+                }
+
+            });
     }
     return (
         <div>
             <div className="container bottom">
                 <h1 className="text-center order-text my-3">Your Orders</h1>
                 <div className="row my-5">
-                {
-                    myOrder.map(data => <MyOrderCart
-                    key={data._id}
-                    send={data}
-                    func={handleDeleteOrder}
-                    ></MyOrderCart> )
-                }
+                    {
+                        myOrder.map(data => <MyOrderCart
+                            key={data._id}
+                            send={data}
+                            func={handleDeleteOrder}
+                        ></MyOrderCart>)
+                    }
                 </div>
             </div>
-            
+
         </div>
     );
 };
