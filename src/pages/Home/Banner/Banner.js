4511f620d9ee0e5b00a31b35e2b2f3d127f64699
shakilkhanSAU <shakilkhan.sau@gmail.com>
2022-02-17 15:12:48 +0600

    aboutus and faq

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,25 +5,21 @@ import '../../style/banner.css';
 
 const Banner = () => {
     return (
-
         <div className='container'>
-
-            
-
-                <div className="row view d-flex justify-content-center align-items-center flex-lg-row flex-column-reverse">
-                    <div className="col-lg-6 col-md-12 col-sm-12">
-                        <h1 className='main-header'>Delicious food for <br />your cravings</h1>
-                        <p className='details'>We made fresh and healty meals with different recipes</p>
-                        <Link to={`/explor`}>
+            <div className="row view d-flex justify-content-center align-items-center flex-lg-row flex-column-reverse">
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                    <h1 className='main-header'>Delicious food for <br />your cravings</h1>
+                    <p className='details'>We made fresh and healty meals with different recipes</p>
+                    <Link to={`/explor`}>
                         <button className='custom-btn'>View Menu</button>
-                        </Link>
-                        
-                    </div>
-                    <div className="col-lg-6 col-md-12 col-sm-12">
-                        <img className='img-fluid' src={banner_img} alt="" />
-                    </div>
+                    </Link>
+
                 </div>
-            
+                <div className="col-lg-6 col-md-12 col-sm-12">
+                    <img className='img-fluid' src={banner_img} alt="" />
+                </div>
+            </div>
+
         </div>
     );
 };
